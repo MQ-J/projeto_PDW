db4ca96584679bff1556b34fa054df010d342b07
MQ-J <mqjunioretecgv@gmail.com>
2022-06-24 09:37:37 -0300

    getUsers deprecated

--- app/Http/Controllers/ReactMobileController.php
+++ app/Http/Controllers/ReactMobileController.php
@@ -4,15 +4,6 @@ namespace App\Http\Controllers;
 
 class ReactMobileController extends Controller
 {
-    
-    public function getUsers()
-    {
-
-        $arquivo = __DIR__ . '/users.json';
-        $users = json_decode(file_get_contents($arquivo));
-        
-        return response()->json($users);
-    }
 
     public function login()
     {
