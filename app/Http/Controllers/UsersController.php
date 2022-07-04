9aa7c3177f4e777d7266153c2c7f41369b658771
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2022-07-04 14:11:17 +0000

    try to use DB

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ use App\Models\User;
 class UsersController extends Controller
 {
     public function index() {
-        return User::all();
+        User::factory(10)->create();
+        User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        return response()->json([
+            'connection' => "OK"
+        ]);
     }
 }
