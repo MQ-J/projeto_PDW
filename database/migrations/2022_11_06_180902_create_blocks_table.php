109ad300ec418976ac12951e14ec9284045b558d
Caique Daniel <caiquedaniel8@gmail.com>
2022-11-11 01:13:49 -0300

    Criação de migrations das tabelas menu e blocos.

---
+++ database/migrations/2022_11_06_180902_create_blocks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId("user")->references("id")
+                ->on("users")
+                ->onDelete("cascade");
+
+            $table->foreignId("menu")->references("id")
+                ->on("menus")
+                ->onDelete("cascade");
+
+            $table->string("text");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+};
