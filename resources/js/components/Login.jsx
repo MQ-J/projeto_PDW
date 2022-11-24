ef45dccc9e7feaf4ab66930fb019aaab5e65ce87
henrique <hi@henriqueo.com>
2022-11-24 16:50:30 -0300

    Reverting test - enabling env verification

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -92,8 +92,7 @@ function NewUserModal(props) {
 
             props.setLoading(true)
 
-            // const url = process.env.NODE_ENV == "development" ? "http://127.0.0.1:8000" : "https://projectpwd.herokuapp.com"
-            const url = "https://projectpwd.herokuapp.com"
+            const url = process.env.NODE_ENV == "development" ? "http://127.0.0.1:8000" : "https://projectpwd.herokuapp.com"
 
             try {
                 await axios.post(`${url}/api/user`, new FormData(event.target));
