95021c11667aca1303fee6d9b99c68a18764d315
henrique souza <hi@henriqueo.com>
2022-11-30 13:31:31 -0300

    Adding menus

--- client/src/components/Menu.svelte
+++ client/src/components/Menu.svelte
@@ -1,15 +1,26 @@
 <script>
-    import { listMenus } from "../services/menu.service";
+    import { listMenus, createMenus } from "../services/menu.service";
+    import { afterUpdate } from "svelte";
 
     import { menuPermalink } from '../store';
+    import Button from "./Button.svelte";
 
     function setMenu(permalink){
         menuPermalink.update(() => permalink)
     }
+
+    let menuList = listMenus();
+
+    function refreshMenus() {
+        menuList = listMenus();
+    }
+
+    afterUpdate(refreshMenus);
+    
 </script>
 <nav>
     <u>
-        {#await listMenus()}
+        {#await menuList}
             Loading menu...
         {:then menuItems}
             <span style="display: none">{setMenu(menuItems.data[0].permalink)}</span>
@@ -19,4 +30,9 @@
             {/each}
         {/await}
     </u>
+    <form on:submit={(e) => {e.preventDefault(); createMenus(e.target.name.value).then(refreshMenus); e.target.name.value=''}}>
+        <h2>New menu item</h2>
+        <input type="text" name="name" class="border p-1 rounded-md" placeholder="Name of the new menu..." >
+        <div  style="width: 50px"><Button>Save</Button></div>
+    </form>
 </nav>
