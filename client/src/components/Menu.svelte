9d262f8327a036bdc1a453e2a907aa9ccd993da3
henrique souza <hi@henriqueo.com>
2022-11-30 13:21:13 -0300

    First menu is default

--- client/src/components/Menu.svelte
+++ client/src/components/Menu.svelte
@@ -12,6 +12,8 @@
         {#await listMenus()}
             Loading menu...
         {:then menuItems}
+            <span style="display: none">{setMenu(menuItems.data[0].permalink)}</span>
+
             {#each menuItems.data as menuItem}
                 <li class="inline-block"><button on:click={() => setMenu(menuItem.permalink)} class="bg-black text-white mr-1 p-1 rounded-md">{menuItem.name}</button></li>
             {/each}
